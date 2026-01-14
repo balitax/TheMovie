28c29004ec5f3d0779bf30cad34a0033753741aa
Agus Cahyono <cahyo.mamen@gmail.com>
2026-01-14 20:18:02 +0700

    Initial commit

---
+++ TheMovie/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TheMovie
+//
+//  Created by Aguscahyo on 14/01/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
